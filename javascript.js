390a58d453650313e445e4eb78a8a457f4600a82
Ayush <abhay27nigam02@gmail.com>
2021-09-17 17:07:56 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if(this.scrollY>20){
+            $('.navbar').addClass("sticky");
+        }
+        else
+        {
+   $('.navbar').removeClass("sticky");
+        }
+
+         if(this.scrollY > 300){
+            $('.scroll-up-btn').addClass("show");
+        }
+        else
+        {
+  $('.scroll-up-btn').removeClass("show");
+        }
+    });
+// slide up 
+ $('.scroll-up-btn').click(function() {
+        $('html').animate({scrollTop:0});
+       
+    });
+    // toggle menu
+    $('.menu-btn').click(function() {
+        $('.navbar .menu').toggleClass("active");
+        $(' .menu-btn i').toggleClass("active");
+    })
+     $('.menu').click(function() {
+        $('.navbar .menu').toggleClass("active");
+        $(' .menu-btn i').toggleClass("active");
+    })
+});
+
+
+// typing animation script
+var typed=new Typed(".typing",{
+    strings:["web developer","Designer","Freelancer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true,
+})
+var typed=new Typed(".typing-2",{
+    strings:["React","Javascript","Express","HTML","CSS","Typescript","MongoDb",],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true,
+})
+
+
+// Scroll to specific values
+// scrollTo is the same
+window.scroll({
+  top: 2500, 
+  left: 0, 
+  behavior: 'smooth'
+});
+
+// Scroll certain amounts from current position 
+window.scrollBy({ 
+  top: 100, // could be negative value
+  left: 0, 
+  behavior: 'smooth' 
+});
+
+
